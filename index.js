1d5f73c434f66707c7be9f21d33008973fcc03ff
Amal S Raj <amalsraj@cet.ac.in>
2017-07-22 14:51:41 +0530

    Demo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cool = require('cool-ascii-faces');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -13,8 +14,11 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/cool', function(request,response){
+  response.send(cool());
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
 
