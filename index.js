ecc13f56b75296cbaf9b1457a44fb460d514f615
Amal S Raj <amalsraj@cet.ac.in>
2017-07-22 23:53:27 +0530

    added a bit of documentation

--- index.js
+++ index.js
@@ -4,27 +4,31 @@ const cheerio = require('cheerio');
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000)); //No idea what this does
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); //I think setting the root directory
 
-// views is directory for all template files
+// views is directory for all template HTML files, won't be needing this for our backend 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/', function(request, response) { //Use pages/index.ejs file when someone GETs "app.com/"
   response.render('pages/index');
 });
 
-app.get('/wordnet', function(request,response){
-  response.header('Access-Control-Allow-Origin','*');
-  console.log('Request Parameter',request.param("q"));
-  console.log('Encoded Request Parameter',encodeURIComponent(request.param('q')));
-  wordnetRequest(request.param('q'),function(str){
-    $ = cheerio.load(str);
-    var detail = $('#detail').html();
-    console.log('Cheerio#detail',detail);
-    response.send(detail);
+//Meat of the app, triggers when someone GETs "app.com/wordnet" or realistically "app.com/wordnet?q=WORD"
+//and calls the function(request,response)
+//Use request variable to see details about the request.
+//Use response variable to set details of the response that should be given out, when a user GETs "app.com/wordnet"
+app.get('/wordnet', function(request,response){ 
+  response.header('Access-Control-Allow-Origin','*');//Enable CORS by adding the Access-Control-Allow-Origin header to the response.
+  console.log('Request Parameter',request.param("q"));//For Testing
+  console.log('Encoded Request Parameter',encodeURIComponent(request.param('q')));//For testing
+  wordnetRequest(request.param('q'),function(str){//Passes WORD from "app.com/wordnet?q=WORD" to wordnetRequest(q,mainCallback) function defined below, and after wordnetRequest is finished, it calls the function(str) defined in this line
+    $ = cheerio.load(str); //str is the entire webpage from www.cfilt.iitb.ac.in/indowordnet/first?langno=9&queryword=WORD, then loaded Cheerio
+    var detail = $('#detail').html(); //Scrape ROUGHLY what we need
+    console.log('Cheerio#detail',detail);//For Testing
+    response.send(detail);//Sends back the scraped html back to the user
   });
   
 });
@@ -36,27 +40,21 @@ app.listen(app.get('port'), function() {
 //HTTP Request to indowordnet:
 
 function wordnetRequest(q, mainCallback) {
-  
-
   var options = {
     host: 'www.cfilt.iitb.ac.in',
     path: '/indowordnet/first?langno=9&queryword=' + encodeURIComponent(q)
   };
-
   callback = function(response) {
     var str = '';
-
     //another chunk of data has been recieved, so append it to `str`
     response.on('data', function (chunk) {
       str += chunk;
     });
-
     //the whole response has been recieved, so we just print it out here
     response.on('end', function () {
       console.log(str.slice(0,50));
       mainCallback(str);
     });
   }
-
   http.request(options, callback).end();
 }
